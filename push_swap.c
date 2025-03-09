07bd19a28bd5a675f33e772b1d0c069c35d6d30f
German G. Lugo Munoz <glugo-mu@42firenze.it>
2025-03-09 07:27:36 +0100

    Add error controls.

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:27:48 by glugo-mu          #+#    #+#             */
-/*   Updated: 2025/03/02 12:54:36 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2025/03/09 07:25:15 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int		ft_atoi(char *s, char *valid)
+int		ft_atoi(char *s)
 {
 	int	sign;
 	int	result;
@@ -34,50 +34,64 @@ int		ft_atoi(char *s, char *valid)
 	{
 		if (*s >= '0' && *s <= '9')
 			result = result * 10 + (*s - '0');
-		else 
-			*valid = '\0';
 		s++;
 	}
 	return (result * sign);
 }
 
-int	validate_no_clones(char **argv) {
-	int i;
-	int j;
+int	validate_no_clones(char **argv)
+{
+	int	i;
+	int	j;
 	
-	i = 0;
-	while (argv[i]) {
+	i = 1;
+	while (argv[i])
+	{
 		j = i + 1;
-		while (argv[j]) {
-			if (strcmp(argv[i], argv[j]) == 0) {
-				printf("Gotcha! %s, %s\n", argv[i], argv[j]);
-				return 1;
+		while (argv[j])
+		{
+			if (strcmp(argv[i], argv[j]) == 0) // remember tu use own ft_strcmp
+			{
+				write(2, "Error\n", 6);
+				exit(1);
 			}
 			j++;
 		}
 		i++;
 	}
-	return 0;
+	return (0);
 }
 
-// int 	validate_no_bad_nums()
-// {
+int	validate_no_characters(char **argv)
+{
+	int	i;
 	
-// 	return (1);
-// }
+	i = 1;
+	while (argv[i])
+	{
+		if (!(ft_atoi(argv[i])))
+		{
+			write(2, "Error\n", 6);
+			exit(1);
+		}
+		i++;
+	}
+		
+	return (1);
+}
 
-int		validate_args(int argc, char **argv)
+int	validate_args(int argc, char **argv)
 {
 	(void)argc;
 	validate_no_clones(argv);
-	// validate_no_bad_nums(**argv);
+	validate_no_characters(argv);
 	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2)
-		return (1);
+	if (argc == 1)
+		return (0);	
 	if (!validate_args(argc, argv))
 		return (1);
 	return (0);
